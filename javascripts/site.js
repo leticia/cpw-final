e006dc8d7bad2873ea7cc12cb91471969027fd0a
Letícia <jlcfigueira@gmail.com>
2011-12-07 00:33:16 -0200

    Thumbs maiores para preenchimento da tela

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,10 +1,22 @@
+/*********************************************
+ * Slider
+*********************************************/
+//Inicializa o slider
 $(document).ready(function(){
   $("#slider").nivoSlider({
     effect: 'fade',
     directionNav: true,
     controlNavThumbs: true
   });
+
+  //Faz com que a última thumbnail tenha margem direita 0
+  $("#slider a.nivo-control:last-child").css('margin-right', '0');
 });
 
-var currentTime = new Date()
-var year = currentTime.getFullYear()
+
+/*********************************************
+ * Rodapé - variáveis
+*********************************************/
+
+var currentTime = new Date();
+var year = currentTime.getFullYear();
