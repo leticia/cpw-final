895bf833fad011f1d1a30ff6fd98860184a1b07c
Fabio-CS <fabio.cs3mx@gmail.com>
2011-12-08 01:16:59 -0200

    ajustes

---
+++ envia_email.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Código para envio de e-mail utilizando a classe PHPMailer
+ *
+ * @author Leo Baiano <leobaiano@leobaiano.com>
+ * @version 1.0
+*/
+ 
+// Chama a classe PHPMailer (pode baixar ela aqui: http://phpmailer.sourceforge.net)
+require("libs/class.phpmailer.php");
+require ("libs/class.smtp.php");
+
+// Instancia o objeto $mail a partir da Classe PHPMailer
+$mail = new PHPMailer();
+$mail->SetLanguage("br", "libs/"); // Linguagem
+
+// Recupera os dados do formulário
+$nome       = $_POST['nome'];
+$telefone   = $_POST['telefone'];
+$email      = $_POST['email'];
+$assunto	= $_POST['assunto'];
+$mensagem	= $_POST['mensagem'];
+//$arquivo    = $_FILES['arquivo']; <-- Não está sendo usado, somente para envio de arquivo.
+ 
+// Recupera o nome do arquivo
+//$arquivo_nome = $arquivo['name']; <-- Não está sendo usado, somente para envio de arquivo.
+ 
+// Recupera o caminho temporario do arquivo no servidor
+//$arquivo_caminho = $arquivo['tmp_name']; <-- Não está sendo usado, somente para envio de arquivo.
+ 
+// Monta a mensagem que será enviada
+$corpo = "
+			<font face='Arial, Helvetica, sans-serif' size='2'>
+			<strong>Nome:</strong> $nome<br />
+			<strong>E-mail:</strong> $email<br />				
+	   		<strong>Telefone:</strong> $telefone<br />
+			<strong>Mensagem enviada:</strong><br /> $mensagem
+			</font>
+	";
+$corpoSimples = "
+            Nome: $nome\n
+			E-mail: $email\n				
+	   		Telefone: $telefone\n
+			Mensagem enviada: \n $mensagem
+	    
+	";
+ 
+// Informo o Host, From, subject e para quem o e-mail será enviado
+//$mail->IsSMTP(); // telling the class to use SMTP
+//$mail->SMTPAuth = true; 
+$mail->CharSet = 'UTF-8';
+$mail->IsMail(true);
+$mail->Host = 'localhost';
+//$mail->Username = 'yoopala@yoopala.com';
+//$mail->Password = 'hugo2612';
+$mail->From = 'site@winget.com.br';
+$mail->FromName = 'Site Winget Creative Solutions';
+$mail->Subject = $assunto;
+$mail->AddAddress('winget@winget.com.br','Contato do Site');
+$mail->AddCC('fabio.cs3mx@gmail.com','Fábio CS');
+// Informa que a mensagem deve ser enviada em HTML
+$mail->IsHTML(true);
+ 
+// Informa o corpo da mensagem
+$mail->Body = $corpo;
+ 
+// Se o e-mail destino não suportar HTML ele envia o texto simples
+$mail->AltBody = $corpoSimples;
+ 
+// Anexa o arquivo
+//$mail->AddAttachment($arquivo_caminho, $arquivo_nome); <-- Não está sendo usado, somente para envio de arquivo.
+ 
+// Tenta enviar o e-mail e analisa o resultado
+
+// Nas versões do PHP anteriores a 4.1.0, deve ser usado $HTTP_POST_FILES
+// ao invés de $_FILES.
+
+/*$uploaddir = '/homez.467/proenfan/www/anexo/'; <-- Não está sendo usado, somente para envio de arquivo.
+$uploadfile = $uploaddir . $_FILES['arquivo']['name'];
+print "<pre>";
+if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $uploaddir . $_FILES['arquivo']['name'])) {
+    print "O arquivo &eacute; v&aacute;lido e foi carregado com sucesso. Informação:\n";
+    print_r($_FILES);
+} else {
+    print "Possivel ataque de upload! Aqui esta alguma informação:\n";
+    print_r($_FILES);
+}
+print "</pre>";*/
+
+if(!$mail->Send()) {
+                echo "Erro: " . utf8_decode($mail->ErrorInfo);
+        } else {
+                header("Location: contato.html?envio=ok");
+        }
+
+?>
--- javascripts/site.js
+++ javascripts/site.js
@@ -1,3 +1,16 @@
+/*******************************
+Google Analytics
+********************************/
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27558064-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 /*********************************************
  * Slider
  *********************************************/
@@ -32,3 +45,11 @@ $(document).ready(function(){
  *********************************************/
 var currentTime = new Date();
 var year = currentTime.getFullYear();
+
+/*$(document).ready(function(){
+	 alert(Utf8.encode('Está página de blog não está com as funcionalidades implementadas ainda.'));
+	});*/
+	
+	function aviso(){
+	alert('Está página de blog não está com as funcionalidades implementadas ainda.');
+	}
